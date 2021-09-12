3c1d41ab9fdfa46224a94935c1c8b56a2c9d4601
KaysuyaTamai <xabi.k0124football@gmail.com>
2021-09-12 11:55:11 +0900

    セル別の音声出力

---
+++ Model/AudioViewController.swift
@@ -0,0 +1,26 @@
+//音源データ管理
+import UIKit
+import AVFoundation
+
+class AudioViewController: UIViewController,AVAudioPlayerDelegate {
+
+    var audioPlayer: AVAudioPlayer!
+    var audioPlayer1: AVAudioPlayer!
+    var audioPlayer2: AVAudioPlayer!
+    var audioPlayer3: AVAudioPlayer!
+    var audioPlayer4: AVAudioPlayer!
+    var audioPlayer5: AVAudioPlayer!
+    var audioPlayer6: AVAudioPlayer!
+    var audioPlayer7: AVAudioPlayer!
+    var audioPlayer8: AVAudioPlayer!
+    var audioPlayer9: AVAudioPlayer!
+    let soundList = ["bell03","small-bell01","bell05","button20","button71","button83","one16","one32","button19"]
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+
+    }
+}
--- View/BiginnerViewController.swift
+++ View/BiginnerViewController.swift
@@ -8,7 +8,8 @@ class BiginnerViewController: UITableViewController,AVAudioPlayerDelegate {
     let titleName: String
     let sentenceView = SentenceViewController()
     let soundsView = SoundsViewController()
-    var audioPlayer: AVAudioPlayer!
+    let AudioVC = AudioViewController()
+    
     
     init(titleName: String) {
         self.titleName = titleName
@@ -38,22 +39,98 @@ class BiginnerViewController: UITableViewController,AVAudioPlayerDelegate {
             make.edges.equalTo(container.contentLayoutGuide)
         }
         //音声データのパス指定
-        let audioPath  = Bundle.main.path(forResource: "bell03", ofType: "mp3")!
+        let audioPath  = Bundle.main.path(forResource: AudioVC.soundList[0], ofType: "mp3")!
         let audioUrl  = URL(fileURLWithPath: audioPath)
+        
+        let audioPath1  = Bundle.main.path(forResource: AudioVC.soundList[1], ofType: "mp3")!
+        let audioUrl1  = URL(fileURLWithPath: audioPath1)
+        
+        let audioPath2  = Bundle.main.path(forResource: AudioVC.soundList[2], ofType: "mp3")!
+        let audioUrl2  = URL(fileURLWithPath: audioPath2)
+        
+        let audioPath3  = Bundle.main.path(forResource: AudioVC.soundList[3], ofType: "mp3")!
+        let audioUrl3 = URL(fileURLWithPath: audioPath3)
+        
+        let audioPath4  = Bundle.main.path(forResource: AudioVC.soundList[4], ofType: "mp3")!
+        let audioUrl4  = URL(fileURLWithPath: audioPath4)
+        
+        let audioPath5  = Bundle.main.path(forResource: AudioVC.soundList[5], ofType: "mp3")!
+        let audioUrl5  = URL(fileURLWithPath: audioPath5)
+        
+        let audioPath6 = Bundle.main.path(forResource: AudioVC.soundList[6], ofType: "mp3")!
+        let audioUrl6  = URL(fileURLWithPath: audioPath6)
+        
+        let audioPath7  = Bundle.main.path(forResource: AudioVC.soundList[7], ofType: "mp3")!
+        let audioUrl7  = URL(fileURLWithPath: audioPath7)
+        
+        let audioPath8 = Bundle.main.path(forResource: AudioVC.soundList[8], ofType: "mp3")!
+        let audioUrl8  = URL(fileURLWithPath: audioPath8)
+        
         //音声データのエラー時処理
         var audioError: NSError?
         do {
-            audioPlayer = try AVAudioPlayer(contentsOf: audioUrl)
+            AudioVC.audioPlayer = try AVAudioPlayer(contentsOf: audioUrl)
         } catch let error as NSError {
             audioError = error
-            audioPlayer = nil
+            AudioVC.audioPlayer = nil
         }
+        
+        do {
+            AudioVC.audioPlayer1 = try AVAudioPlayer(contentsOf: audioUrl1)
+        } catch let error as NSError {
+            audioError = error
+            AudioVC.audioPlayer1 = nil
+        }
+        
+        do {
+            AudioVC.audioPlayer2 = try AVAudioPlayer(contentsOf: audioUrl2)
+        } catch let error as NSError {
+            audioError = error
+            AudioVC.audioPlayer2 = nil
+        }
+        do {
+            AudioVC.audioPlayer3 = try AVAudioPlayer(contentsOf: audioUrl3)
+        } catch let error as NSError {
+            audioError = error
+            AudioVC.audioPlayer3 = nil
+        }
+        do {
+            AudioVC.audioPlayer4 = try AVAudioPlayer(contentsOf: audioUrl4)
+        } catch let error as NSError {
+            audioError = error
+            AudioVC.audioPlayer4 = nil
+        }
+        do {
+            AudioVC.audioPlayer5 = try AVAudioPlayer(contentsOf: audioUrl5)
+        } catch let error as NSError {
+            audioError = error
+            AudioVC.audioPlayer5 = nil
+        }
+        do {
+            AudioVC.audioPlayer6 = try AVAudioPlayer(contentsOf: audioUrl6)
+        } catch let error as NSError {
+            audioError = error
+            AudioVC.audioPlayer6 = nil
+        }
+        do {
+            AudioVC.audioPlayer7 = try AVAudioPlayer(contentsOf: audioUrl7)
+        } catch let error as NSError {
+            audioError = error
+            AudioVC.audioPlayer7 = nil
+        }
+        do {
+            AudioVC.audioPlayer8 = try AVAudioPlayer(contentsOf: audioUrl8)
+        } catch let error as NSError {
+            audioError = error
+            AudioVC.audioPlayer8 = nil
+        }
+  
         
         if let error = audioError {
             print("Error\(error.localizedDescription)")
         }
         
-        audioPlayer.delegate = self
+        AudioVC.audioPlayer.delegate = self
         
         tableView.dataSource = self
         tableView.delegate  = self
@@ -73,7 +150,7 @@ class BiginnerViewController: UITableViewController,AVAudioPlayerDelegate {
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         //CustomTableViewCellの追加
         let cell = tableView.dequeueReusableCell(withIdentifier: "cell") as! CustomTableViewCell
-        //cellの文字
+        //cellの文字指定
         cell.setCell(sentence: sentenceView.sentence[indexPath.row], pronunciation: sentenceView.Pronunciation[indexPath.row], japanese: sentenceView.japanese[indexPath.row
         ])
         
@@ -85,10 +162,30 @@ class BiginnerViewController: UITableViewController,AVAudioPlayerDelegate {
     }
     //cellをタップした時の処理
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        print("selected")
-        audioPlayer.play()
-        
+
+        if indexPath.row == 0 {
+            AudioVC.audioPlayer.play()
+        } else if indexPath.row == 1{
+            AudioVC.audioPlayer1.play()
+        } else if indexPath.row == 2{
+            AudioVC.audioPlayer2.play()
+        } else if indexPath.row == 3{
+            AudioVC.audioPlayer3.play()
+        } else if indexPath.row == 4{
+            AudioVC.audioPlayer4.play()
+        } else if indexPath.row == 5{
+            AudioVC.audioPlayer5.play()
+        } else if indexPath.row == 6{
+            AudioVC.audioPlayer6.play()
+        } else if indexPath.row == 7{
+            AudioVC.audioPlayer7.play()
+        } else if indexPath.row == 8{
+            AudioVC.audioPlayer8.play()
+        } else {
+            print("ミス")
+        }
     }
-    
-    
+        
 }
+    
+    
