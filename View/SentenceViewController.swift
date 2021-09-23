af55ae4273315852b7c8a2225a808401b1fd6cd2
KaysuyaTamai <xabi.k0124football@gmail.com>
2021-09-23 12:36:50 +0900

    add_IntermediateSentence add_AdvanceSentence

--- View/SentenceViewController.swift
+++ View/SentenceViewController.swift
@@ -3,11 +3,25 @@ import UIKit
 
 class SentenceViewController: UITabBarController {
 
-    var sentence = ["你早","下午好","晚上好","再见/再会","明天见","下周见","回头见","你好吗？","谢谢","对不起"]
+    var sentence = ["你早","下午好","晚上好","再见","明天见","下周见","回头见","你好吗？","谢谢","对不起"]
     
-    var Pronunciation = ["Nǐzǎo","Xiàwǔ hǎo.","Wǎnshang hǎo","Zàijiàn /Zàihuì","Míngtiān jiàn","Xiàzhōu jiàn","Huítou jiàn","Nǐ hǎo ma?","Xièxie","duìbuqǐ",]
+    var Pronunciation = ["Nǐzǎo","Xiàwǔ hǎo.","Wǎnshang hǎo","Zàijiàn","Míngtiān jiàn","Xiàzhōu jiàn","Huítou jiàn","Nǐ hǎo ma?","Xièxie","duìbuqǐ",]
     
     var japanese = ["おはよう","こんにちは","こんばんは","さようなら","また明日","また来週","またあとで","お元気ですか？","ありがとう","ごめんなさい",]
+    
+    
+    var IntermediateSentence = ["我","你","他","她","我们","他们","她们","这","这些","那","那些"]
+    
+    var IntermediatePronunciation = ["wǒ","nǐ","tā","tā","wǒmen","tāmen","tāmen","zhè","zhèxiē","nà","nàxiē"]
+    
+    var IntermediateJananase = ["わたし","あなた","彼","女性","わたしたち","彼ら","彼女たち","これ","これら","それ","それら   "]
+    
+    
+    var AdvancedSentence = ["","","","","","","","","",""]
+    
+    var AdvancedPronunciation = ["","","","","","","","","",""]
+    
+    var AdvancedJananase = ["","","","","","","","","",""]
     
     override func viewDidLoad() {
         super.viewDidLoad()
