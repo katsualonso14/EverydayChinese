8c71dc0a59b41ce399bd174ec9495bd663038be3
KaysuyaTamai <xabi.k0124football@gmail.com>
2021-10-04 20:32:14 +0900

    layout_制約追加

--- View/CategoryViewController.swift
+++ View/CategoryViewController.swift
@@ -30,34 +30,58 @@ class CategoryViewController: UIViewController {
     //    初心者ボタン
     func biginnerButton() {
         
-        let button:UIButton = UIButton(frame: CGRect(x: 70, y: 150, width: 300, height: 150))
+        let button:UIButton = UIButton()
+        self.view.addSubview(button)
+        //Autosizingのレイアウトの仕組みをAuto Layoutに変換するかどうかを設定するフラグを既存がオンなのでfalse(オフ)に設定
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.backgroundColor = .yellow
+        //connstraitでlayoutを調整する
+        button.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 150.0).isActive = true
+        button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        button.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
+        button.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.15).isActive = true
         button.setTitle("初心者", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.addTarget(self, action: #selector(pushBignnerButton), for: .touchUpInside)
         button.layer.cornerRadius = 20.0
         button.layer.masksToBounds = true
-        self.view.addSubview(button)
+       
 
     }
     //    中級者ボタン
     func intermediateButton() {
         
-        let button:UIButton = UIButton(frame: CGRect(x: 70, y: 350, width: 300, height: 150))
+        let button:UIButton = UIButton()
+        self.view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.backgroundColor = .orange
+        //connstraitでlayoutを調整する
+        button.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 350.0).isActive = true
+        button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        button.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
+        button.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.15).isActive = true
         button.setTitle("中級者", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.addTarget(self, action: #selector(pushIntermediateButton), for: .touchUpInside)
         button.layer.cornerRadius = 20.0
         button.layer.masksToBounds = true
-        self.view.addSubview(button)
-
+        
     }
     //    上級者ボタン
     func advancedButton() {
         
-        let button:UIButton = UIButton(frame: CGRect(x: 70, y: 550, width: 300, height: 150))
+        let button:UIButton = UIButton()
+        self.view.addSubview(button)
+        button.translatesAutoresizingMaskIntoConstraints = false
         button.backgroundColor = .blue
+        //connstraitでlayoutを調整する
+        button.topAnchor.constraint(equalTo: self.view.topAnchor, constant: 550.0).isActive = true
+        button.centerXAnchor.constraint(equalTo: self.view.centerXAnchor).isActive = true
+        button.centerYAnchor.constraint(equalTo: self.view.centerYAnchor).isActive = true
+        button.widthAnchor.constraint(equalTo: self.view.widthAnchor, multiplier: 0.8).isActive = true
+        button.heightAnchor.constraint(equalTo: self.view.heightAnchor, multiplier: 0.15).isActive = true
         button.setTitle("上級者", for: .normal)
         button.setTitleColor(.black, for: .normal)
         button.addTarget(self, action: #selector(pushAdvancedButton), for: .touchUpInside)
