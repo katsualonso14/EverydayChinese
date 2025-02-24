a3b0a678a79dbde3bf736969dc71864b317abde0
katsuyatamai <xabi.k0124football@gmail.com>
2025-02-24 10:28:35 +0100

    Fix Cell Corner Radius

--- View/Parts/QuickMemoCell.swift
+++ View/Parts/QuickMemoCell.swift
@@ -3,10 +3,24 @@ import UIKit
 class QuickMemoCell: UITableViewCell {
     
     let label = UILabel()
+
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        // layer
+        self.layer.cornerRadius = 16
+        self.layer.masksToBounds = true
+        self.backgroundColor = .systemBackground
+        // contentView layer
+        self.contentView.layer.cornerRadius = 16
+        self.contentView.layer.masksToBounds = true
+        self.contentView.backgroundColor = .systemBackground
+        
+        self.contentView.layer.borderColor = UIColor.systemGray6.cgColor
+        self.contentView.layer.borderWidth = 3
+    }
     
     override init(style: UITableViewCell.CellStyle, reuseIdentifier: String?) {
         super.init(style: style, reuseIdentifier: reuseIdentifier)
-        
         label.textColor = AppColors.textColor
         label.translatesAutoresizingMaskIntoConstraints = false
         contentView.addSubview(label)
